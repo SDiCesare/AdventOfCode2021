e717c8768d744f57e9f60897915a513ca77918ed
SDiCesare <SDiCesare@users.noreply.github.com>
2021-12-04 13:03:08 +0100

    Day 5 preparation

---
+++ day5/es1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> getFileContent(std::ifstream *inFile)
+{
+    std::vector<std::string> out;
+    std::string line;
+    while (std::getline(*inFile, line))
+    {
+        out.push_back(line);
+    }
+    return out;
+}
+
+int main(int argc, char *argv[])
+{
+    std::ifstream *inFile = new std::ifstream();
+    std::string fileName;
+    if (argc < 2)
+    {
+        printf("No file passed! Using default File.\n");
+        fileName = "data.txt";
+    }
+    else
+    {
+        fileName = argv[1];
+    }
+    std::cout << "Reading Data from FIle: " << fileName << "\n";
+    inFile->open(fileName);
+    std::vector<std::string> content = getFileContent(inFile);
+    inFile->close();
+    for (int i = 0; i < content.size(); i++)
+    {
+        std::string var = content[i];
+        std::cout << var << "\n";
+    }
+    return 0;
+}
